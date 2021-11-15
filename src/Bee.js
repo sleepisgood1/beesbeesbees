a37e78eb9dbaf02fdc8b4ff7be6fdac8aa8ab7db
sleepisgood1 <jae1994krw@gmail.com>
2021-11-15 11:04:37 -0800

    Assignment Complete

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  Grub.call(this);
+  //creating a new isntance of Grub to call all of its properties for Bee to use
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+//calling all of the grub's methods
+//so by doing the above line, are we setting the cosntructor to be Grub so we have to reset the constsructor?
+Bee.prototype.constructor = Bee;
+//resetting the constructor back to bee
